2820d3ec98a07971377183581562bce50dd4de42
Derek Chavez <frrenose17@yahoo.com>
2023-09-05 13:43:01 -0600

    feat: add flash messages to create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,26 @@ class UsersController <ApplicationController
     @user = User.find(params[:id])
   end 
 
-  def create 
-    user = User.create(user_params)
-    if user.save
-      redirect_to user_path(user)
-    else  
-      flash[:error] = user.errors.full_messages.to_sentence
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    elsif User.exists?(email: params[:user][:email])
+      flash[:error] = "Email has already been taken"
       redirect_to register_path
-    end 
-  end 
+    elsif params[:user][:password] != params[:user][:password_confirmation]
+      flash[:error] = "Password and Password Confirmation does not match."
+      redirect_to register_path
+    else
+      flash[:error] = "You must fill in all fields."
+      redirect_to register_path
+    end
+  end
 
   private 
 
   def user_params 
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
 end 
